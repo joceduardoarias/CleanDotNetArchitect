c4e09a7683bdebe2823b788d2bf814ccb241fbb8
Jose Arias <josee.ariasmelo@gmail.com>
2023-12-14 17:09:09 -0300

    Feature/ add models

---
+++ CleanArchitecture/CleanArchitecture.Data/Streamer.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CleanArchitecture.Data
+{
+    public class Streamer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set;}        
+
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Data/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Data
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int StreamerId { get; set; }
+        public virtual Streamer? Streamer { get; set; }
+    }
+}
