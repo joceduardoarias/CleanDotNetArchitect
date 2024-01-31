5b529044d59988c2942d3a452c0dbf7fbb47db5d
Jose Arias <josee.ariasmelo@gmail.com>
2024-01-31 18:21:47 -0300

    Behaviours y MediatR
    - Se agregan excepciones para manejar las validaciones generadas con FluentValidation en el pipeline.
    - Se agregan excepciones para controlar errores en los métodos handle de los command y query.

---
+++ CleanArchitecture/CleanArchitecture.Application/Behaviuors/UnHandledExceptionBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.Behaviuors;
+
+public class UnHandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public UnHandledExceptionBehaviour(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogError(ex, "Application Request: Sucedio una excepcion para el request {Name} {@Request}", requestName, request);
+            throw;
+        }
+    }
+}
+/* El objetivo es analizar los métodos hadle de los commands y queries*/
